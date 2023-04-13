6c53b20950c94f86af11c34a978286390170e3d5
Katarzyna Marek <kasia@marek.net>
2023-04-13 20:01:35 +0200

    review fixes

--- src/main/scala/net/marek/tyre/MooreMachine.scala
+++ src/main/scala/net/marek/tyre/MooreMachine.scala
@@ -31,7 +31,7 @@ case class Compose[T1 <: Tuple, T2 <: Tuple, T3 <: Tuple](r1: Routine[T1, T2], r
   def execOn(stack: T1, c: Char): T3 = r2.execOn(r1.execOn(stack, c), c)
 
 
-trait StateWithRoutine[I <: Tuple, R <: Tuple]:
+trait StateWithRoutine[I <: Tuple, R]:
   type O <: Tuple
   def stateS: State[O, R]
   def routine: Routine[I, O]
@@ -43,31 +43,30 @@ trait StateWithRoutine[I <: Tuple, R <: Tuple]:
       def stack = newStack
     }
 
-trait State[S <: Tuple, R <: Tuple]:
+sealed trait State[S <: Tuple, R]:
   def next(c : Char): List[StateWithRoutine[S, R]]
 
-trait AcceptingState[R <: Tuple] extends State[R, R]:
-  def next(c : Char): List[StateWithRoutine[R, R]]
+sealed trait AcceptingState[R] extends State[R *: EmptyTuple, R]:
+  def next(c : Char): List[StateWithRoutine[R *: EmptyTuple, R]]
 
-trait Thread[R <: Tuple]:
+trait Thread[R]:
   type S <: Tuple
   def state: State[S, R]
   def stack: S
   def next(c : Char): List[Thread[R]] =
     state.next(c).map(_.thread(stack, c))
   def getIfAccepting: Option[R] =
-    state match {
-      case _ : AcceptingState[_] => Some(stack)
+    state match
+      case _ : AcceptingState[_] =>
+        Some(stack.asInstanceOf[R *: EmptyTuple](0))
       case _ => None
-    }
 
 trait MooreMachine[R]:
-  val initStates : List[StateWithRoutine[EmptyTuple, R *: EmptyTuple]]
+  val initStates : List[StateWithRoutine[EmptyTuple, R]]
   def parse(word : List[Char]): Option[R] =
-    def parseRec(word : List[Char], threads : List[Thread[R *: EmptyTuple]]): Option[R] =
+    def parseRec(word : List[Char], threads : List[Thread[R]]): Option[R] =
       word match
         case c :: rest => parseRec(rest, threads.flatMap(_.next(c)))
-        case Nil => threads.map(_.getIfAccepting).collect{
-          case Some(value) => value._1
-        }.headOption
+        case Nil => threads.map(_.getIfAccepting).collectFirst:
+          case Some(value) => value
     parseRec(word, initStates.map(_.thread(Tuple.apply(), 'a')))
