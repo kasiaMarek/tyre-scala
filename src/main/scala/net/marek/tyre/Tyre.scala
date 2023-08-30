1073bc272b0dace871c364761e6dc13435621d4c
Katarzyna Marek <kasia@marek.net>
2023-08-30 22:39:29 +0200

    enrich tyre parser
    Co-Authored-By: Robert Marek <64277069+susuro@users.noreply.github.com>

--- src/main/scala/net/marek/tyre/Tyre.scala
+++ src/main/scala/net/marek/tyre/Tyre.scala
@@ -1,11 +1,11 @@
 // Tyre
 sealed trait Tyre[R]:
-	def rep = Star(this)
-	def <*>[S](re: Tyre[S]) = And(this, re)
-	def <|>[S](re: Tyre[S]) = Or(this, re)
-	def map[S](f: R => S) = Conv(this, f)
+  def rep = Star(this)
+  def <*>[S](re: Tyre[S]) = And(this, re)
+  def <|>[S](re: Tyre[S]) = Or(this, re)
+  def map[S](f: R => S) = Conv(this, f)
 case class OneOf(cs: List[Char]) extends Tyre[Char]:
-	override def toString: String = s"OneOf($cs)"
+  override def toString: String = s"OneOf($cs)"
 case class Or[R1, R2](right: Tyre[R1], left: Tyre[R2]) extends Tyre[Either[R1, R2]]
 case class And[R1, R2](right: Tyre[R1], left: Tyre[R2]) extends Tyre[(R1, R2)]
 case class Star[R](re: Tyre[R]) extends Tyre[List[R]]
@@ -13,13 +13,24 @@ case object Epsilon extends Tyre[Unit]
 case class Conv[R1, R2](tyre: Tyre[R1], f: R1 => R2) extends Tyre[R2]
 
 object Tyre:
-	def char(c : Char) = OneOf(List(c))
-	given Conversion[Char, Tyre[Char]] = char(_)
+  def char(c: Char) = OneOf(List(c))
+  given Conversion[Char, Tyre[Char]] = char(_)
 
 //Re
 sealed trait Re
-case class ReOneOf(cs: List[Char]) extends Re
-case class ReOr(right: Re, left: Re) extends Re
-case class ReAnd(right: Re, left: Re) extends Re
-case class ReStar(re: Re) extends Re
-case object ReEpsilon extends Re
+case class ReOneOf(cs: List[Char]) extends Re:
+  override def toString(): String = cs match
+    case c :: Nil => c.toString()
+    case list => s"[${list.mkString}]"
+
+case class ReOr(right: Re, left: Re) extends Re:
+  override def toString(): String = s"($right|$left)"
+case class ReAnd(right: Re, left: Re) extends Re:
+  override def toString(): String = s"($right$left)"
+case class ReStar(re: Re) extends Re:
+  override def toString(): String = s"$re*"
+case object ReEpsilon extends Re:
+  override def toString(): String = "_"
+
+object Re:
+  def char(c: Char) = ReOneOf(List(c))
--- src/test/scala/net/marek/tyre/StringParserTest.scala
+++ src/test/scala/net/marek/tyre/StringParserTest.scala
@@ -1,26 +1,33 @@
 import org.scalatest.funsuite.AnyFunSuite
+import Re.char
 
 class StringParserTest extends AnyFunSuite:
 
-	test("Simple parser"):
-		println(TyreParser(""))
-		println(TyreParser("x"))
-		println(TyreParser("(x)"))
-		println(TyreParser("xy"))
-		println(TyreParser("x|y"))
-		println(TyreParser("(x|y)"))
-		println(TyreParser("x*"))
-		println(TyreParser("(x(y|a))b"))
-		println(TyreParser("x(y|a)b"))
-		println(TyreParser("x|y*"))
-		println(TyreParser("x)y"))
-		println(TyreParser("x|*"))
-		println(TyreParser("(x*y)*"))
-		assertCompiles("""tyre"x"""")
-		assertDoesNotCompile("""tyre"x|*"""")
-		val t = tyre"x|(xh)"
-		val m: "x" = "x"
-		val h = tyre"$m"
-		println(t)
-		// val good = tyre"x"
-		// val bad = tyre"(aa"
+  def assertDoesNotParse(pattern: String) =
+    assert(TyreParser(pattern).isEmpty)
+
+  def assertParses(pattern: String, result: Re) =
+    assertResult(Some(result), pattern)(TyreParser(pattern))
+
+  test("Simple parser"):
+    assertDoesNotParse("")
+    assertParses("x", char('x'))
+    assertParses("(x)", char('x'))
+    assertParses("xy", ReAnd(char('x'), char('y')))
+    assertParses("x|y", ReOr(char('x'), char('y')))
+    assertParses("(x|y)", ReOr(char('x'), char('y')))
+    assertParses("x*", ReStar(char('x')))
+    assertParses("x?", ReOr(char('x'), ReEpsilon))
+    assertParses("(x(y|a))b", ReAnd(ReAnd(char('x'), ReOr(char('y'), char('a'))), char('b')))
+    assertParses("xy|a", ReOr(ReAnd(char('x'), char('y')), char('a')))
+    assertParses("x|ya", ReOr(char('x'), ReAnd(char('y'), char('a'))))
+    assertParses("x(y|a)b", ReAnd(char('x'), ReAnd(ReOr(char('y'), char('a')), char('b'))))
+    assertParses("x|y*", ReOr(char('x'), ReStar(char('y'))))
+    assertParses("(x*y)*", ReStar(ReAnd(ReStar(char('x')), char('y'))))
+    assertParses("x\\)", ReAnd(char('x'), char(')')))
+    assertParses("x*\\*y\\\\", ReAnd(ReStar(char('x')), ReAnd(char('*'), ReAnd(char('y'), char('\\')))))
+    assertParses("[s-v]", ReOneOf(List('s', 't', 'u', 'v')))
+    assertDoesNotParse("x)y")
+    assertDoesNotParse("x|*")
+    assertCompiles("""tyre"x"""")
+    assertDoesNotCompile("""tyre"x|*"""")
