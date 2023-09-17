6d403d8f79367c37e4a43ff4459d0cfa8dead005
Robert Marek <robert.marek@fingo.info>
2023-09-17 07:41:14 +0200

    Enhance tests

--- src/test/scala/net/marek/tyre/StringParserTest.scala
+++ src/test/scala/net/marek/tyre/StringParserTest.scala
@@ -2,6 +2,7 @@ package net.marek.tyre
 
 import org.scalatest.funsuite.AnyFunSuite
 import Re.char
+import java.time.LocalTime
 
 class StringParserTest extends AnyFunSuite:
 
@@ -43,8 +44,33 @@ class StringParserTest extends AnyFunSuite:
     assertResult(Some(None))(m.run(""))
     assertResult(None)(m.run("aa"))
 
+  test("Time parser"):
+    def digit(c: Char): Int = c - '0'
+    def decimal(c: Char, p: Int): Int = digit(c)*10.pow(p)
+    def number(cs: Char*): Int =
+      cs.reverse.zipWithIndex.map(decimal(_, _)).sum
+    val ht = tyre"[0-1][0-9]|2[0-3]".map: e =>
+      val (h10, h1) = e.merge
+      number(h10, h1)
+    val mt = tyre"[0-5][0-9]".map(number(_, _))
+    val tt = tyre"$ht:$mt".map: t =>
+      val (h, (_, m)) = t
+      LocalTime.of(h, m)
+    val tm = tt.compile()
+    val time = tm.run("21:25")
+    assertResult(Some(LocalTime.of(21, 25)))(time)
+    assertResult(None)(tm.run("52:12"))
+    assertResult(None)(tm.run("22:"))
+    assertResult(None)(tm.run("22:222"))
+    assertResult(None)(tm.run("x"))
+
   def tokenize(str: String): List[Token] =
     str.toList.zipWithIndex.map {
       case ('@', idx) => Hole(idx)
       case (e, _) => e
     }
+
+  extension (n: Int)
+    def pow(p: Int): Int = p match
+      case x if x > 0 => n * n.pow(x-1)
+      case _ => 1
