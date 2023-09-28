c65210ba588f0d0f5472b15606ac30034b5d6bb2
Robert Marek <robert.marek@fingo.info>
2023-09-28 22:13:00 +0200

    Enhance tests. Provide methods to inspect Moore machine form - wip.
    Co-authored-by: kasiaMarek <kasia@marek.net>

--- src/main/scala/net/marek/tyre/Interfaces.scala
+++ src/main/scala/net/marek/tyre/Interfaces.scala
@@ -2,9 +2,13 @@ package net.marek.tyre
 
 trait Machine[T]:
   def run(str: String): Option[T]
+  def getAll(str: String): List[T]
+  def show(cs: Set[Char]): String
 
 extension [T](regex: Tyre[T])
   def compile(): Machine[T] =
     new Machine:
       val mm = MMConstruction(Context[T *: EmptyTuple]).compile(regex)
       def run(str: String): Option[T] = mm.parse(EmptyTuple, str.toCharArray().toList).map(_.head)
+      def getAll(str: String) = mm.parseAll(EmptyTuple, str.toCharArray().toList).map(_.head)
+      def show(cs: Set[Char]): String = mm.show(cs)
--- src/test/scala/net/marek/tyre/RegexTest.scala
+++ src/test/scala/net/marek/tyre/RegexTest.scala
@@ -6,6 +6,17 @@ import java.time.LocalTime
 
 class RegexTest extends AnyFunSuite:
 
+  test("Star problem"):
+    val t = tyre"[A-Za-z0-9_]"
+    val tt = tyre"($t*)*".map: x =>
+      //println(Thread.currentThread().getStackTrace().mkString("\n\t"))
+      println(x)
+      x
+    val tm = tt.compile()
+    val sample = "abc.xyz"
+    // println(tm.getAll(sample))
+    println(tm.show(sample.toSet))
+
   test("Time parser"):
     val ht = tyre"[0-1][0-9]|2[0-3]".map(number(_, _))
     val mt = tyre"[0-5][0-9]".map(number(_, _))
@@ -20,10 +31,71 @@ class RegexTest extends AnyFunSuite:
     assertResult(None)(tm.run("22:222"))
     assertResult(None)(tm.run("x"))
 
+  test("Email parser"):
+    // Dash escaping doesn't work
+    // val ldudt = tyre"[A-Za-z0-9_\\-]"
+    val lddut = tyre"[A-Za-z0-9_]"
+    // val lddt = tyre"[A-Za-z0-9\\-]"
+    val lddt = tyre"[A-Za-z0-9]"
+    val ldt = tyre"[A-Za-z0-9]"
+    val lt = tyre"[A-Za-z]"
+    // This causes exception - but shouldn't
+    // val ut = tyre"${lddut}${lddut}*(.${lddut}${lddut}*)*".map: t =>
+    //   val (t1, t2, t3) = t
+    //   val t3s = t3.map{ case (t31, t32, t33) => s"${t31}${t32}${t33.mkString}" }
+    //   s"${t1}${t2.mkString}$t3s"
+    val ut = tyre"${lddut}${lddut}*(.${lddut}${lddut}*)*".map(string) // user (local) part
+    val sdt = tyre"${ldt}(${lddt}*${ldt})?".map(string) // subdomain element
+    val tdt = tyre"$lt$lt$lt*".map(string)  // top domain
+    val dt = tyre"$sdt.($sdt.)*$tdt".map(string)  // domain part
+    val et = tyre"$ut@$dt"  // email
+    val tm = et.compile()
+    val email = tm.run("robert.marek@fingo.net")
+    assert(email.isDefined)
+    val (user, _, domain) = email.get
+    assertResult("robert.marek")(user)
+    assertResult("fingo.net")(domain)
+    assertResult(None)(tm.run("robert.marek"))
+    assertResult(None)(tm.run("@fingo.net"))
+    assertResult(None)(tm.run("robert@marek"))
+    assertResult(None)(tm.run(".robert@fingo.net"))
+
+  test("Money parser"):
+    // type Symbol = '$' | '€' |'£' | '₣' | '₿'
+    type Symbol = Char
+    case class Money(amount: BigDecimal, currency: Symbol)
+    val pt = tyre"[$$€£₣₿] ?[1-9][0-9]*(.[0-9][0-9])?"
+    val st = tyre"[1-9][0-9]*(.[0-9][0-9])? ?[$$€£₣₿]"
+    val mt = tyre"$pt|$st".map: e =>
+      val t = e.fold(
+        l => (l(0), l(2) :: l(3), l(4)),
+        r => (r(4), r(0) :: r(1), r(2))
+      )
+      val integral = number(t(1)*)
+      val fraction = t(2).map(p => (number(p(1), p(2))))
+      (t(0), bigDecimal(integral, fraction, 2))
+    val tm = mt.compile()
+    val amount = tm.run("€1643.52").map(t => Money(t(1), t(0)))
+    assertResult(Some(Money(BigDecimal(1643.52), '€')))(amount)
+
   def digit(c: Char): Int = c - '0'
-  def decimal(c: Char, p: Int): Int = digit(c)*10.pow(p)
+  def decimal(c: Char, p: Int): Int =
+    digit(c)*10.pow(p)
   def number(cs: Char*): Int =
       cs.reverse.zipWithIndex.map(decimal(_, _)).sum
+  def bigDecimal(int: Int, fract: Option[Int], scale: Int): BigDecimal =
+    val mult = 10.pow(scale)
+    ((BigDecimal(int) * BigDecimal(mult) + BigDecimal(fract.getOrElse(0))) / BigDecimal(mult)).setScale(scale)
+  def string(x: Any): String = x match
+    case h *: t => s"${string(h)}${string(t)}"
+    case h :: t => s"${string(h)}${string(t)}"
+    case Some(x) => s"${string(x)}"
+    case Left(x) => s"${string(x)}"
+    case Right(x) => s"${string(x)}"
+    case EmptyTuple => ""
+    case Nil => ""
+    case None => ""
+    case s => s.toString
 
   extension (n: Int)
     def pow(p: Int): Int = p match
