bb03d5f02acafca932b39ae1c49dfc9931c352f2
Robert Marek <robert.marek@fingo.info>
2023-11-12 20:38:57 +0100

    Fix formatting

--- src/main/scala/net/marek/tyre/NumberHelper.scala
+++ src/main/scala/net/marek/tyre/NumberHelper.scala
@@ -4,7 +4,7 @@ object NumberHelper:
 
   extension (n: Int)
     def pow(p: Int): Int = p match
-      case x if x > 0 => n * n.pow(x-1)
+      case x if x > 0 => n * n.pow(x - 1)
       case _ => 1
 
   def digit(c: Char): Int = c - '0'
@@ -13,5 +13,5 @@ object NumberHelper:
   private def hexAt(c: Char, pos: Int): Int = digit(c) * 16.pow(pos)
   def hex(cs: Char*): Int = cs.reverse.zipWithIndex.map(hexAt(_, _)).sum
 
-  private def decimalAt(c: Char, pos: Int): Int = digit(c)*10.pow(pos)
+  private def decimalAt(c: Char, pos: Int): Int = digit(c) * 10.pow(pos)
   def decimal(cs: Char*): Int = cs.reverse.zipWithIndex.map(decimalAt(_, _)).sum
